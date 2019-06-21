8e827af4feda1311e323f3025b030866012038d5
Paul Martin <communty.hivemind@gmail.com>
2019-06-21 13:54:47 +0100

    Update sort 0s and 1s

--- ruby/algos/sort_0s_and_1s.rb
+++ ruby/algos/sort_0s_and_1s.rb
@@ -1,16 +1,31 @@
 def sort_0s_and_1s(input)
-  output = []
-  index_0 = 0
-  index_1 = input.length - 1
+  output = [nil] * input.length
+  i_left = 0
+  i_right = input.length - 1
+
   input.each do |thing|
     if thing == 0
-      output[index_0] = 0
-      index_0 += 1
-    end
-    if thing == 1
-      output[index_1] = 1
-      index_1 -= 1
+      output[i_left] = 0
+      i_left += 1
+    else
+      output[i_right] = 1
+      i_right -= 1
     end
   end
+
   output
+end
+
+if __FILE__ == $0
+  require_relative '../lib/timing_stuffs'
+
+  input_to_algo = Proc.new { |n| (([0] * (n/2)) + ([1] * (n/2))).shuffle }
+  algo = Proc.new { |input| sort_0s_and_1s(input) }
+
+  timings = get_timings(max: 10_000,
+                        iterations: 1000,
+                        input_to_algo: input_to_algo,
+                        algo: algo)
+
+  save_as_csv(timings)
 end
